b9b028b6234f9140f3ba602872369ec6553fbf66
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2022-09-20 15:25:23 +0100

    config: setup react + esbuild

---
+++ assets/js/App.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+
+export default function App(_params) {
+
+    return (
+        <h1> it renders </h1>
+    )
+}
--- config/config.exs
+++ config/config.exs
@@ -1,10 +1,3 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Config module.
-#
-# This configuration file is loaded before any dependency and
-# is restricted to this project.
-
-# General application configuration
 import Config
 
 config :fnd,
@@ -23,7 +16,7 @@ config :esbuild,
   version: "0.14.29",
   default: [
     args:
-      ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
+      ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/* --loader:.js=jsx),
     cd: Path.expand("../assets", __DIR__),
     env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
   ]
