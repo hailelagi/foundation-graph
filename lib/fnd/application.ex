dba828374c175a031a597ff3daaaaa747633e2f8
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2022-09-20 15:41:27 +0100

    chore: cleanup

--- lib/fnd/application.ex
+++ lib/fnd/application.ex
@@ -1,33 +1,19 @@
 defmodule Fnd.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
-  @moduledoc false
-
   use Application
 
   @impl true
   def start(_type, _args) do
     children = [
-      # Start the Ecto repository
       Fnd.Repo,
-      # Start the Telemetry supervisor
       FndWeb.Telemetry,
-      # Start the PubSub system
       {Phoenix.PubSub, name: Fnd.PubSub},
-      # Start the Endpoint (http/https)
       FndWeb.Endpoint
-      # Start a worker by calling: Fnd.Worker.start_link(arg)
-      # {Fnd.Worker, arg}
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Fnd.Supervisor]
     Supervisor.start_link(children, opts)
   end
 
-  # Tell Phoenix to update the endpoint configuration
-  # whenever the application is updated.
   @impl true
   def config_change(changed, _new, removed) do
     FndWeb.Endpoint.config_change(changed, removed)
--- mix.exs
+++ mix.exs
@@ -14,9 +14,6 @@ defmodule Fnd.MixProject do
     ]
   end
 
-  # Configuration for the OTP application.
-  #
-  # Type `mix help compile.app` for more information.
   def application do
     [
       mod: {Fnd.Application, []},
@@ -28,9 +25,6 @@ defmodule Fnd.MixProject do
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
-  # Specifies your project dependencies.
-  #
-  # Type `mix help deps` for examples and options.
   defp deps do
     [
       {:phoenix, "~> 1.6.12"},
@@ -47,16 +41,12 @@ defmodule Fnd.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:neuron, "~> 5.0.0"}
+      {:neuron, "~> 5.0.0"},
+      # compile time audit for security vulns
+      {:sobelow, "~> 0.8", only: :dev}
     ]
   end
 
-  # Aliases are shortcuts or tasks specific to the current project.
-  # For example, to install project dependencies and perform other setup tasks, run:
-  #
-  #     $ mix setup
-  #
-  # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
       setup: ["deps.get", "ecto.setup"],
