acfa93a132d36343de15edb40befd16f02a4d078
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2022-09-20 22:46:12 +0100

    test: client mock data

---
+++ lib/fnd/graph/client_mock.ex
@@ -0,0 +1,233 @@
+defmodule Fnd.Graph.ClientMock do
+  def query_nfts(_) do
+    {:ok,
+     [
+       %{
+         "dateMinted" => "1645445845",
+         "id" => "0x00013154b142dccdca9eed000c8ed435d07efced-1",
+         "tokenIPFSPath" => "QmbknnBAkvMXWic6u7cPUb9Pq5NhGJhkF442npUKkzP45z/metadata.json"
+       },
+       %{
+         "dateMinted" => "1645691898",
+         "id" => "0x000366fd8b5a6d3acdbb46b286e56c7396c51daa-1",
+         "tokenIPFSPath" => "QmfN6wzr51yVq3kGEACG6oBHA24rC1E1YqmBmTERmjvyKD/metadata.json"
+       },
+       %{
+         "dateMinted" => "1645693101",
+         "id" => "0x000366fd8b5a6d3acdbb46b286e56c7396c51daa-2",
+         "tokenIPFSPath" => "QmTLFoqtxMP14iCFnpUU7wop8Ujiag1wNTN3CqxxYmuLYg/metadata.json"
+       },
+       %{
+         "dateMinted" => "1650745806",
+         "id" => "0x00047d0d2a0afad9aedc5e840533df8336f85e4e-1",
+         "tokenIPFSPath" => "QmP6EBLxNCwVWE7vmmGaPbrwQGGWRsPKzjGYMh8NaL5Wtn/metadata.json"
+       },
+       %{
+         "dateMinted" => "1650848293",
+         "id" => "0x00047d0d2a0afad9aedc5e840533df8336f85e4e-2",
+         "tokenIPFSPath" => "QmRsFaYhKZzKa9WxN87WXjhnXB6mKvENxnidYRi1Me5a8S/metadata.json"
+       },
+       %{
+         "dateMinted" => "1652527298",
+         "id" => "0x00047d0d2a0afad9aedc5e840533df8336f85e4e-3",
+         "tokenIPFSPath" => "QmP1a9ZNs2gpwN6diJiPtQKVQ5owVrkDwqjh4ugThKLnMp/metadata.json"
+       },
+       %{
+         "dateMinted" => "1651598301",
+         "id" => "0x00054ad9b9afa9728b139e950861d81e3e57d75a-1",
+         "tokenIPFSPath" => "QmZFPwd4LLYp2okzwjxJLCP3vcGqsfCCX4f1iezveH6V6T/metadata.json"
+       },
+       %{
+         "dateMinted" => "1651621196",
+         "id" => "0x00054ad9b9afa9728b139e950861d81e3e57d75a-2",
+         "tokenIPFSPath" => "QmPiPkPKmb2SkUxMRbzVzhoaooTL34hSNtsJEAPocDwEfr/metadata.json"
+       },
+       %{
+         "dateMinted" => "1653421197",
+         "id" => "0x00054ad9b9afa9728b139e950861d81e3e57d75a-3",
+         "tokenIPFSPath" => "Qmb1jrWJotupPSEnMNXDJ5FLNRfh7XMxdX6fCMUfBnWtwU/metadata.json"
+       },
+       %{
+         "dateMinted" => "1653427152",
+         "id" => "0x00054ad9b9afa9728b139e950861d81e3e57d75a-4",
+         "tokenIPFSPath" => "QmXHKcAn9D7PXbdp5V6MPSxvFTcUCJcgtMyWR8dzgKi4j5/metadata.json"
+       },
+       %{
+         "dateMinted" => "1653694803",
+         "id" => "0x00054ad9b9afa9728b139e950861d81e3e57d75a-5",
+         "tokenIPFSPath" => "QmS6jfGVnod2jg8TY5NZj3g5mukboPSXy4oQNqwsPUPcKa/metadata.json"
+       },
+       %{
+         "dateMinted" => "1653837165",
+         "id" => "0x00054ad9b9afa9728b139e950861d81e3e57d75a-6",
+         "tokenIPFSPath" => "QmdVWC7Er8KMdaQUHg1AnYmW3nBEEhKpQpPzybaMz8f7EA/metadata.json"
+       },
+       %{
+         "dateMinted" => "1653842292",
+         "id" => "0x00054ad9b9afa9728b139e950861d81e3e57d75a-7",
+         "tokenIPFSPath" => "Qma9hs2nqyDCzzvt5maKHtpxzkxqg5HpTm3h5YDTPGwwyo/metadata.json"
+       },
+       %{
+         "dateMinted" => "1651765712",
+         "id" => "0x000651454b81d641cdad21b7416e33cfc22583b4-1",
+         "tokenIPFSPath" => "QmT8UDw9jVw716t6Nr37JRqTaAeTh5uZ4HGeZYxvb3cWVw/metadata.json"
+       },
+       %{
+         "dateMinted" => "1655739902",
+         "id" => "0x000699917c0886f82ee0824f9cce7dc91e8a97fa-1",
+         "tokenIPFSPath" => "QmPbdoV7MVwEjyNZPMUwKm3y7JSStR6jfbXT9ewTkGnx6p/metadata.json"
+       },
+       %{
+         "dateMinted" => "1647490050",
+         "id" => "0x0009f3ec0cfaa84e09817710423f353cdb029186-1",
+         "tokenIPFSPath" => "QmeCjMwYDZi1919ZoX3weQXvY4S6S8REwKwVzzR8csAVE8/metadata.json"
+       },
+       %{
+         "dateMinted" => "1643107612",
+         "id" => "0x000b0a3b69b5ddb9e07984a764f89542bb2c67ef-1",
+         "tokenIPFSPath" => "QmR9SdnQJNsY2G5d59YRXtyjBzgi6ovpdTLZbRSFkGhx6k/metadata.json"
+       },
+       %{
+         "dateMinted" => "1643108590",
+         "id" => "0x000b0a3b69b5ddb9e07984a764f89542bb2c67ef-2",
+         "tokenIPFSPath" => "QmZbNaUpUK9aNdLRzjrcA89ReNKJhhREEMKTejmTeEqZdZ/metadata.json"
+       },
+       %{
+         "dateMinted" => "1661064776",
+         "id" => "0x000c0ff45b6c697ce5a9ee724ea0a9c4f95dd0ec-1",
+         "tokenIPFSPath" => "QmWSPyLJVTYfGER7Xa7XfWXDrBC6n6pzqh7JFYUUx5TUTV/metadata.json"
+       },
+       %{
+         "dateMinted" => "1661068353",
+         "id" => "0x000c0ff45b6c697ce5a9ee724ea0a9c4f95dd0ec-2",
+         "tokenIPFSPath" => "QmW2trChnjWrbECU78zXqXiLL89awDVa59k2waSeF6E8yU/metadata.json"
+       },
+       %{
+         "dateMinted" => "1661238002",
+         "id" => "0x000c0ff45b6c697ce5a9ee724ea0a9c4f95dd0ec-3",
+         "tokenIPFSPath" => "QmUu8Rbbh1WoEXEH6GTWAkRyMnKqwbAKFdfBQh6ekDFMv2/metadata.json"
+       },
+       %{
+         "dateMinted" => "1661721773",
+         "id" => "0x000c0ff45b6c697ce5a9ee724ea0a9c4f95dd0ec-4",
+         "tokenIPFSPath" => "QmaW8si7zvCFofxX5kf4XyH76RCf4xvbucXYzacKHoPgKE/metadata.json"
+       },
+       %{
+         "dateMinted" => "1662113618",
+         "id" => "0x000c0ff45b6c697ce5a9ee724ea0a9c4f95dd0ec-5",
+         "tokenIPFSPath" => "QmWi5b5kYaY1uhbttzV9fj8r4UDva2D4w7SgQzRBAjA1LH/metadata.json"
+       },
+       %{
+         "dateMinted" => "1661110044",
+         "id" => "0x000c5b85714fe40b4af10cca4b504b47e1d5c9dc-10",
+         "tokenIPFSPath" =>
+           "https://ipfs.pixura.io/ipfs/QmbcoQpbUkzo7AXg8WiKUngNetJy9YBAnbW8RBdhAY5Db5/metadata.json"
+       },
+       %{
+         "dateMinted" => "1651920324",
+         "id" => "0x000d2b3f269d81eaa66bb1f1cd6e7f816675a71f-1",
+         "tokenIPFSPath" => "QmVTf5etmQ7NvuLE6uupi4Z6P6Uah2QroPTNzckQeMVRr4/metadata.json"
+       },
+       %{
+         "dateMinted" => "1652011848",
+         "id" => "0x000d2b3f269d81eaa66bb1f1cd6e7f816675a71f-2",
+         "tokenIPFSPath" => "QmWyF2FXbwypYJHV7fRTDJDS7SpNdSno8rwUG4FXsvR88Y/metadata.json"
+       },
+       %{
+         "dateMinted" => "1652455782",
+         "id" => "0x000d2b3f269d81eaa66bb1f1cd6e7f816675a71f-3",
+         "tokenIPFSPath" => "QmattMkM4D916p8LufZNfLgZJ27GyxpH6RCqLB8SdUQwaD/metadata.json"
+       },
+       %{
+         "dateMinted" => "1647500165",
+         "id" => "0x000e8bfbd84f0a8e0d29066fced400785c34e6ef-1",
+         "tokenIPFSPath" => "QmRw1N5nVHg3boHtDLUMjKtHYqHxXJYGahnmmn5hYucPpv/metadata.json"
+       },
+       %{
+         "dateMinted" => "1644143935",
+         "id" => "0x0013432c57211ec388973fff80ad7e7425c1804a-1",
+         "tokenIPFSPath" => "QmdTCtV9GFnzAUg9nnGZqH25fG6oz6N3DqW6A3y94ky7BG/metadata.json"
+       },
+       %{
+         "dateMinted" => "1655837907",
+         "id" => "0x0013432c57211ec388973fff80ad7e7425c1804a-10",
+         "tokenIPFSPath" => "QmQggpxoDNj19sDcKMiUx1NX63KT14S2QBDe6XvUMuShAa/metadata.json"
+       },
+       %{
+         "dateMinted" => "1655838386",
+         "id" => "0x0013432c57211ec388973fff80ad7e7425c1804a-11",
+         "tokenIPFSPath" => "QmWpNHT9fkZU38GyFmdXC3shr6Y8UhLRNDepi8L2RSp1ZD/metadata.json"
+       },
+       %{
+         "dateMinted" => "1655838807",
+         "id" => "0x0013432c57211ec388973fff80ad7e7425c1804a-12",
+         "tokenIPFSPath" => "Qmc96VfBWm9KrVtP1KCXESeqfHTkpG3aLzBCTebDbK36iE/metadata.json"
+       },
+       %{
+         "dateMinted" => "1656608119",
+         "id" => "0x0013432c57211ec388973fff80ad7e7425c1804a-13",
+         "tokenIPFSPath" => "QmexJ54nYMorVuL14LYhG9JiQq1FCDCFHUanC4kfgJ2ZGK/metadata.json"
+       },
+       %{
+         "dateMinted" => "1659414529",
+         "id" => "0x0013432c57211ec388973fff80ad7e7425c1804a-14",
+         "tokenIPFSPath" => "QmXqwWmS9ZpPNjDWHdnmWoMTSpRHznbe5qm8aQvNrCADNz/metadata.json"
+       },
+       %{
+         "dateMinted" => "1659414992",
+         "id" => "0x0013432c57211ec388973fff80ad7e7425c1804a-15",
+         "tokenIPFSPath" => "QmPmNtW5FR7aA2TkpVhkkbRjSYfRK7PPQ49P6ozC9hmyUD/metadata.json"
+       },
+       %{
+         "dateMinted" => "1659415273",
+         "id" => "0x0013432c57211ec388973fff80ad7e7425c1804a-16",
+         "tokenIPFSPath" => "QmWPpNiNLW77HE5gXAQdcfELEpue4NfM1YcgMrPLGZErFB/metadata.json"
+       },
+       %{
+         "dateMinted" => "1660062737",
+         "id" => "0x0013432c57211ec388973fff80ad7e7425c1804a-17",
+         "tokenIPFSPath" => "QmNUMexwF9k8S31snKYLnkEe7VVyqXCVBQH2qnVxqHq9fJ/metadata.json"
+       },
+       %{
+         "dateMinted" => "1662881536",
+         "id" => "0x0013432c57211ec388973fff80ad7e7425c1804a-18",
+         "tokenIPFSPath" => "QmRMjxRGRGH8eKzNgVnXoJXwKzjDSHythSJm4AJLHP9Cfq/metadata.json"
+       },
+       %{
+         "dateMinted" => "1644652668",
+         "id" => "0x0013432c57211ec388973fff80ad7e7425c1804a-2",
+         "tokenIPFSPath" => "QmeGP4D71kVjBMokGhPLqt8utaE2eQP56iR2FTdky4gffn/metadata.json"
+       },
+       %{
+         "dateMinted" => "1644655456",
+         "id" => "0x0013432c57211ec388973fff80ad7e7425c1804a-3",
+         "tokenIPFSPath" => "Qmab3mD9SkrWTEtAf7BYK9X1Rx5wC9FVsZeiinD98ukZXt/metadata.json"
+       },
+       %{
+         "dateMinted" => "1647196481",
+         "id" => "0x0013432c57211ec388973fff80ad7e7425c1804a-4",
+         "tokenIPFSPath" => "QmRaesjrPW3kuDEXV8NhyukzxDPtEJ9xxDf6sBozY2o3vq/metadata.json"
+       },
+       %{
+         "dateMinted" => "1647789496",
+         "id" => "0x0013432c57211ec388973fff80ad7e7425c1804a-5",
+         "tokenIPFSPath" => "QmP8N2AxiMWy1gPcg1Q8qWpG2xvGWwUoueepk9m1Ps7nwt/metadata.json"
+       },
+       %{
+         "dateMinted" => "1648827418",
+         "id" => "0x0013432c57211ec388973fff80ad7e7425c1804a-6",
+         "tokenIPFSPath" => "QmTuud47XsmJNGW3AQKJg4EdNiQfYKFpG5seauTDUwCr6o/metadata.json"
+       },
+       %{
+         "dateMinted" => "1649778256",
+         "id" => "0x0013432c57211ec388973fff80ad7e7425c1804a-7",
+         "tokenIPFSPath" => "QmYtxHcfdDvWSxmNViALuaiovUqtDU8Tb2UzaYYe5mbfJs/metadata.json"
+       },
+       %{
+         "dateMinted" => "1652222997",
+         "id" => "0x0013432c57211ec388973fff80ad7e7425c1804a-8",
+         "tokenIPFSPath" => "QmNhHjQp8ECpBAYPxLcibsUmwxy2Wwwk4Vvi7cV758sf7F/metadata.json"
+       }
+     ]}
+  end
+end
