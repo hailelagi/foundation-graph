a41fee0b67d0af56b5482fd1fd6b3fb057e74ffc
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2022-09-22 04:50:18 +0100

    chore: seed database

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,20 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias Fnd.Nft
+
+{:ok, nfts} = Fnd.Graph.ClientMock.query_nfts(nil)
+
+Enum.map(nfts, fn nft ->
+  date = String.to_integer(nft["dateMinted"]) |> DateTime.from_unix!()
+
+  Nft.changeset(
+    %Nft{},
+    %{
+      graph_id: nft["id"],
+      ipfs: nft["tokenIPFSPath"],
+      create_date: date
+    }
+  )
+end)
+|> Enum.reduce(fn n, _acc -> Fnd.Repo.insert!(n) end)
