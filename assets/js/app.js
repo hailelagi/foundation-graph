f03764ba084a54edd81cd590a57b67b75b20928c
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2022-09-22 15:51:41 +0100

    chore: cleanup

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,32 +1,9 @@
 import "../css/app.css"
 import "phoenix_html"
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from "react-dom";
 import App from './App.jsx';
-
-// Establish Phoenix Socket and LiveView configuration.
-import {Socket} from "phoenix"
-import {LiveSocket} from "phoenix_live_view"
-import topbar from "../vendor/topbar"
-
-let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
-
-// Show progress bar on live navigation and form submits
-topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
-window.addEventListener("phx:page-loading-start", info => topbar.show())
-window.addEventListener("phx:page-loading-stop", info => topbar.hide())
-
-// connect if there are any LiveViews on the page
-liveSocket.connect()
-
-// expose liveSocket on window for web console debug logs and latency simulation:
-// >> liveSocket.enableDebug()
-// >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
-// >> liveSocket.disableLatencySim()
-window.liveSocket = liveSocket
 
 // React 18
 const container = document.getElementById('app');
-const root = createRoot(container);
-root.render(<App tab="home" />);
+ReactDOM.render(<App />, container)
